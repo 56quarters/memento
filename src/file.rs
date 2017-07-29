f1971270378efd31bbe50c7965ac6304c2b03ad4
Nick Pillitteri <nick@tshlabs.org>
2017-07-29 15:42:20 -0400

    Silly rabbit, longs are 32 bits in C

--- src/file.rs
+++ src/file.rs
@@ -8,6 +8,7 @@ pub struct WhisperFile {
 }
 
 
+// 16 + (12 * num)
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Header {
     metadata: Metadata,
@@ -16,7 +17,7 @@ pub struct Header {
 
 
 #[derive(Debug, Serialize, Deserialize)]
-#[repr(u64)]
+#[repr(u32)]
 pub enum AggregationType {
     Average = 1,
     Sum = 2,
@@ -28,21 +29,21 @@ pub enum AggregationType {
     AbsMin = 8,
 }
 
-
+// 4 + 4 + 4 + 4 = 16
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Metadata {
     aggregation: AggregationType,
-    max_retention: u64,
+    max_retention: u32,
     x_files_factor: f32,
-    archive_count: u64,
+    archive_count: u32,
 }
 
-
+// 4 + 4 + 4 = 12
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ArchiveInfo {
-    offset: u64,
-    seconds_per_point: u64,
-    num_points: u64,
+    offset: u32,
+    seconds_per_point: u32,
+    num_points: u32,
 }
 
 
@@ -60,6 +61,35 @@ pub struct Archive {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Point {
-    timestamp: u64,
+    timestamp: u32,
     value: f64,
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::{WhisperFile, Header, AggregationType, Metadata,
+                ArchiveInfo, Data, Archive, Point};
+
+
+    #[test]
+    fn test_serialize_to_json() {
+        let p1 = Point { timestamp: 1501355048, value: 45.9 };
+        let p2 = Point { timestamp: 1501355058, value: 30.8 };
+        let p3 = Point { timestamp: 1501355068, value: 46.0 };
+        let p4 = Point { timestamp: 1501355078, value: 35.3 };
+
+        let a1 = Archive { points: vec![p1, p2, p3, p4] };
+        let d1 = Data { archives: vec![a1] };
+
+        let m1 = Metadata {
+            aggregation: AggregationType::Max,
+            max_retention: 86400,
+            x_files_factor: 0.3,
+            archive_count: 1,
+        };
+
+
+
+    }
 }
