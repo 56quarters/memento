c67ffff14f9ce7b6fbb61a690e6d6d2f3053d374
Nick Pillitteri <nick@tshlabs.org>
2017-08-13 19:21:36 -0400

    Tests for file parsing

--- tests/lib.rs
+++ tests/lib.rs
@@ -32,66 +32,29 @@ fn test_parse_header_mean() {
 }
 
 #[test]
-fn test_parse_header_count() {
-    let bytes = include_bytes!("count_01.wsp");
-    let res = whisper_parse_header(bytes);
-    let header = res.unwrap().1;
-    let meta = header.metadata();
-    let info = header.archive_info();
-
-    assert_eq!(AggregationType::Sum, meta.aggregation());
-    assert_eq!(SECONDS_PER_YEAR, meta.max_retention());
-    assert_eq!(5, meta.archive_count());
-
-    assert_eq!(10, info[0].seconds_per_point());
-    assert_eq!(60, info[1].seconds_per_point());
-    assert_eq!(300, info[2].seconds_per_point());
-    assert_eq!(600, info[3].seconds_per_point());
-    assert_eq!(3600, info[4].seconds_per_point());
-
-    assert_eq!(8640, info[0].num_points());
-    assert_eq!(10080, info[1].num_points());
-    assert_eq!(8640, info[2].num_points());
-    assert_eq!(25920, info[3].num_points());
-    assert_eq!(8760, info[4].num_points());
-}
-
-#[test]
-fn test_parse_header_upper() {
-    let bytes = include_bytes!("upper_01.wsp");
-    let res = whisper_parse_header(bytes);
-    let header = res.unwrap().1;
-    let meta = header.metadata();
-    let info = header.archive_info();
-
-    assert_eq!(AggregationType::Max, meta.aggregation());
-    assert_eq!(SECONDS_PER_YEAR, meta.max_retention());
-    assert_eq!(5, meta.archive_count());
-
-    assert_eq!(10, info[0].seconds_per_point());
-    assert_eq!(60, info[1].seconds_per_point());
-    assert_eq!(300, info[2].seconds_per_point());
-    assert_eq!(600, info[3].seconds_per_point());
-    assert_eq!(3600, info[4].seconds_per_point());
-
-    assert_eq!(8640, info[0].num_points());
-    assert_eq!(10080, info[1].num_points());
-    assert_eq!(8640, info[2].num_points());
-    assert_eq!(25920, info[3].num_points());
-    assert_eq!(8760, info[4].num_points());
-}
-
-#[test]
 fn test_parse_file_mean() {
     let bytes = include_bytes!("mean_01.wsp");
-}
+    let res = whisper_parse_file(bytes);
+    let file = res.unwrap().1;
+    let header = file.header();
+    let data = file.data();
 
-#[test]
-fn test_parse_file_count() {
-    let bytes = include_bytes!("count_01.wsp");
-}
+    let meta = header.metadata();
+    let info = header.archive_info();
+    let archives = data.archives();
 
-#[test]
-fn test_parse_file_upper() {
-    let bytes = include_bytes!("upper_01.wsp");
+    assert_eq!(8640, info[0].num_points());
+    assert_eq!(8640, archives[0].points().len());
+
+    assert_eq!(10080, info[1].num_points());
+    assert_eq!(10080, archives[1].points().len());
+
+    assert_eq!(8640, info[2].num_points());
+    assert_eq!(8640, archives[2].points().len());
+
+    assert_eq!(25920, info[3].num_points());
+    assert_eq!(25920, archives[3].points().len());
+
+    assert_eq!(8760, info[4].num_points());
+    assert_eq!(8760, archives[4].points().len());
 }
