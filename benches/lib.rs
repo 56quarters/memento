33387363283ef1e38b33a377170e18144c2bb678
Nick Pillitteri <nick@tshlabs.org>
2017-08-29 23:12:44 -0400

    Move allocation out of benchmark loop

--- benches/lib.rs
+++ benches/lib.rs
@@ -25,10 +25,11 @@ fn bench_whisper_parse_file(b: &mut test::Bencher) {
 fn bench_whisper_encode_header(b: &mut test::Bencher) {
     let bytes = &include_bytes!("../tests/mean_01.wsp")[0..76];
     let header = whisper_parse_header(bytes).unwrap().1;
+    let mut buf = Vec::with_capacity(bytes.len());
 
     b.iter(|| {
-        let mut buf = Vec::with_capacity(bytes.len());
         whisper_encode_header(&mut buf, &header).unwrap();
+                buf.clear();
     });
 }
 
@@ -37,10 +38,11 @@ fn bench_whisper_encode_header(b: &mut test::Bencher) {
 fn bench_whisper_encode_file(b: &mut test::Bencher) {
     let bytes = &include_bytes!("../tests/mean_01.wsp")[..];
     let file = whisper_parse_file(bytes).unwrap().1;
+    let mut buf = Vec::with_capacity(bytes.len());
 
     b.iter(|| {
-        let mut buf = Vec::with_capacity(bytes.len());
         whisper_encode_file(&mut buf, &file).unwrap();
+        buf.clear();
     });
 }
 
