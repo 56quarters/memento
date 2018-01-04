62cc4b767d1e18d23d8965d8d30f3433774b1989
Nick Pillitteri <nick@tshlabs.org>
2018-01-03 22:00:45 -0500

    Get rid of footer

--- cabi/build.rs
+++ cabi/build.rs
@@ -5,8 +5,7 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     cbindgen::Builder::new()
-        .with_header("/* begin generated memento header (see cabi/build.rs) */")
-        .with_trailer("/* end generated memento header (see cabi/build.rs) */")
+        .with_header("/* generated memento header (see cabi/build.rs) */")
         .with_include_guard("MEMENTO_H_INCLUDED")
         .with_language(cbindgen::Language::C)
         .with_crate_and_name(crate_dir, "memento-cabi")
