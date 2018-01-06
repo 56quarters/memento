d44c4d5bdde8c9d6d91d05adb941e91b7f7d7d38
Nick Pillitteri <nick@tshlabs.org>
2018-01-06 13:56:21 -0500

    Actually do something with results

--- cabi/test.c
+++ cabi/test.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <time.h>
-#include "include/memento.h"
+#include "memento.h"
 
 int main(int argc, char **argv) {
     time_t now = time(NULL);
     MementoResult res = memento_fetch_path("../tests/count_01.wsp", 100, now);
+
+    if (memento_result_is_error(&res)) {
+        fprintf(stderr, "Failure getting results!\n");
+        memento_result_free(&res);
+        return 1;
+    }
+
+    for (int i = 0; i < res.size; i++) {
+        printf("%u: %f\n", res.results[i].timestamp, res.results[i].value);
+    }
+
     memento_result_free(&res);
     return 0;
 }
